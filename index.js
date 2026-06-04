60467fc7d0ed33fe061e89381a5344d95f346b0b
NITOH Hisashi <support@amus.biz>
2026-06-04 16:05:36 +0900

    花火のアニメーション表示を球体から放射状に光る様に変更

--- index.js
+++ index.js
@@ -15,11 +15,11 @@ var canvas = document.getElementById("canvas");
 var context = canvas.getContext("2d");
 
 var currentLightness = 0;
+var currentValue = 0;
 var fadeDirection = 1;
 var hue = random(0, 360);
-var x = [];
-var y = [];
-var radius = [];
+var angle = [];
+var distance = [];
 
 /**
  * 乱数
@@ -43,28 +43,28 @@ function resizeCanvas() {
  * パーティクルの初期化
  */
 function initParticles() {
-    hue = random(0, 360);
     for (var i = 0; i < 2000; i++) {
-        var angle = random(0, Math.PI * 2);
-        var distance = Math.sqrt(random(0, 1)) * Math.min(canvas.width, canvas.height) * 0.47;
-        x[i] = Math.cos(angle) * distance + canvas.width / 2;
-        y[i] = Math.sin(angle) * distance + canvas.height / 2;
-        radius[i] = random(1, 300) / 100;
+        angle[i] = random(0, Math.PI * 2);
+        distance[i] = Math.sqrt(random(0, 1)) * Math.min(canvas.width, canvas.height) * 0.47;
     }
 }
 
 /**
  * フレームを描画する
  * @param {number} lightness 明度
+ * @param {number} value 値（花火の成長を表す）
  */
-function drawFrame(lightness) {
+function drawFrame(lightness, value) {
     // 背景
     context.fillStyle = bgColor;
     context.fillRect(0, 0, canvas.width, canvas.height);
 
     for (var i = 0; i < 2000; i++) {
         context.beginPath();
-        context.arc(x[i], y[i], radius[i], 0, Math.PI * 2);
+        var v = Math.min(value / 50, 1) * distance[i]
+        var x = Math.cos(angle[i]) * v + canvas.width / 2;
+        var y = Math.sin(angle[i]) * v + canvas.height / 2;
+        context.arc(x, y, lightness / 20, 0, Math.PI * 2);
         context.fillStyle = "hsl(" + hue + ", 100%, " + lightness + "%)";
         context.fill();
     }
@@ -89,16 +89,19 @@ function getLightness(baseValue) {
  * アニメーションループ
  */
 function animate() {
-    drawFrame(getLightness(currentLightness));
+    drawFrame(getLightness(currentLightness), currentValue);
 
     currentLightness += fadeDirection;
+    currentValue += 1;
     if (currentLightness >= LIGHTNESS_MAX) {
         currentLightness = LIGHTNESS_MAX;
         fadeDirection = -1;
     } else if (currentLightness < 1) {
         currentLightness = 0;
+        currentValue = 0;
         fadeDirection = 1;
         initParticles();
+        hue = random(0, 360);
     }
 
     setTimeout(animate, 50);
